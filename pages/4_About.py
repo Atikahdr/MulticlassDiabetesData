c21c856c2bf0a19628af0bdc21187a21df21a396
Atikahdr <atikahdr12@gmail.com>
2026-08-22 22:11:09 +0700

    Initial commit

--- pages/4_About.py
+++ pages/4_About.py
@@ -1,7 +1,6 @@
 import streamlit as st
 
 from components.load_css import load_css
-from utils.theme import inject_theme
 from components.navbar import navbar
 from components.about import about_page
 from components.footer import footer
@@ -13,7 +12,6 @@ st.set_page_config(
     initial_sidebar_state="collapsed"
 )
 
-inject_theme()
 load_css()
 navbar()
 about_page()
